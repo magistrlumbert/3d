ff285a3deda88d6817590949e678ac89b2709876
Konstantin Shevchenko <dev.shevchenko.konstantin@gmail.com>
2021-07-16 21:39:30 +0300

    remove redirects from toml

--- api/src/index.js
+++ api/src/index.js
@@ -15,7 +15,7 @@ const gateway = new ApolloGateway({
   // prevents composition failures at runtime using schema validation using
   // real usage-based metrics.
   serviceList: [
-    { name: 'bgi', url: 'http://localhost:4001/graphql' },
+    // { name: 'bgi', url: 'http://localhost:4001/graphql' },
     { name: 'casic', url: 'http://localhost:4002/graphql' },
     // { name: 'casc', url: 'http://localhost:4003/graphql' },
     // { name: 'casics3', url: 'http://localhost:4004/graphql' },
